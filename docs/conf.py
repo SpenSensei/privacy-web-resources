825da1a8bd7d6bd75c4eefaf808f2e3da4fe4271
Lityarcea <pitersontoomy@gmail.com>
2026-01-09 16:33:06 +0100

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+project = "Privacy & Web Resources"
+author = "SpenSensei"
+
+extensions = [
+    "myst_parser",
+]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+master_doc = "index"
+
+html_theme = "sphinx_rtd_theme"
